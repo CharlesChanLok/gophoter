ec7925a612db26f22acc4cdc1f38362ebfe423a9
Telford Ho <telfordho@gmai.com>
2018-04-15 00:02:34 +0800

    trying to redux component test to App.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -9,22 +9,22 @@ import {
 } from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import { connect } from 'react-redux';
-// import MainScreen from './src/components/MainScreen';
-// import LoginScreen from './src/components/LoginScreen';
-import { addNum, minNum } from './src/store/actions/numberExport'
+import { Testing } from './src/components/test';
+
 
 
 class App extends Component {
 
   render() {
     return (
+      <Testing/>
       // <AppStackNavigator/>
       // <LoginScreen/>
-      <View>
-      <Text>{this.props.displaynumber}</Text>
-      <Button title="Add" onPress={this.props.OnAdd} />
-      <Button title="Min" onPress={this.props.OnMin} />
-      </View>
+      // <View>
+      // <Text>{this.props.displaynumber}</Text>
+      // <Button title="Add" onPress={this.props.OnAdd} />
+      // <Button title="Min" onPress={this.props.OnMin} />
+      // </View>
     );
   }
 }
@@ -45,17 +45,3 @@ const styles = StyleSheet.create({
   },
 });
 
-const mapStateToProps = state => {
-  return {
-    displaynumber: state.numbers.counter
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return{
-    OnAdd: () => dispatch({type: 'ADD_NUM'}),
-    OnMin: () => dispatch({type: 'MIN_NUM'})
-  }
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(App)
---
+++ Frontend/src/components/test.js
@@ -0,0 +1,61 @@
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { connect } from 'react-redux';
+// import MainScreen from './src/components/MainScreen';
+// import LoginScreen from './src/components/LoginScreen';
+import { addNum, minNum } from '../store/actions/numberExport'
+
+
+export class Testing extends Component {
+
+  render() {
+    return (
+      // <AppStackNavigator/>
+      // <LoginScreen/>
+      <View>
+      <Text>{this.props.displaynumber}</Text>
+      <Button title="Add" onPress={this.props.OnAdd} />
+      <Button title="Min" onPress={this.props.OnMin} />
+      </View>
+    );
+  }
+}
+
+// const AppStackNavigator = StackNavigator ({
+//   Main: {
+//     screen: MainScreen
+//   }
+// })
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+});
+
+const mapStateToProps = state => {
+  return {
+    displaynumber: state.numbers.counter
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return{
+    OnAdd: () => dispatch({type: 'ADD_NUM'}),
+    OnMin: () => dispatch({type: 'MIN_NUM'})
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Testing)
